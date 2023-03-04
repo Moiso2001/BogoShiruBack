c6126de5207b17b7c5fe55fa5ac544c97f8bf14b
MoisoDavid <wollxi@gmail.com>
2023-03-04 11:45:40 -0500

    Cors and .env paths settled

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+import { ConfigService } from '@nestjs/config/dist';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CORS } from './constants';
+import * as morgan from "morgan"
 
 async function bootstrap() {
+
+  /* Initialization of our NestFactory */
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /* Middleware to show errors and each consult in our terminal */
+  app.use(morgan('dev'));
+
+  /* .env variable */
+  const configService = app.get(ConfigService);
+
+  /* CORS options brought from our constants folder */
+  app.enableCors(CORS);
+
+  /* Prefix used in our api route, this will help to set versions ex: localhost:3001/(prefix)/(routes) */
+  app.setGlobalPrefix('beta');
+
+  /* Run app listening our port from our .env file */
+  await app.listen(configService.get('PORT'));
+  console.log(`Application running on: ${await app.getUrl()}`)
 }
 bootstrap();
