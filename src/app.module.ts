c96bc3abb3dbb58fcc2f7eb905028cc42f949b11
MoisoDavid <wollxi@gmail.com>
2023-03-12 15:41:52 -0500

    DB Connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,28 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import mongoConfig from './config/mongo.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: `.${process.env.NODE_ENV}.env`,
-    isGlobal: true
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+      load: [mongoConfig]
+    }), 
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('mongo.uri')
+      }),
+      inject: [ConfigService],
+    })
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
 
+export class AppModule {}
