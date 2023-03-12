24b9023d9c0d0b8daa03c9ffd560a5fe6b7fb857
MoisoDavid <wollxi@gmail.com>
2023-03-12 15:49:57 -0500

    Little Documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,20 +7,28 @@ import mongoConfig from './config/mongo.config';
 
 @Module({
   imports: [
+
+    /* We create and config our ConfigModule, passing envpath and load where we "load" our object with the env values */
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`,
       isGlobal: true,
       load: [mongoConfig]
     }), 
+
+    /* Here is the conection with our mongo uri, the uri already have the password and username in there */
     MongooseModule.forRootAsync({
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('mongo.uri')
       })
-    })
+    }),
+
+    
+
   ],
 
   controllers: [AppController],
+
   providers: [AppService],
 })
 
