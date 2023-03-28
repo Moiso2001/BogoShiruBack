d307de7adb6a617ccfeecc8d428ddfb80c3804fd
MoisoDavid <wollxi@gmail.com>
2023-03-28 10:57:31 -0500

    Controllers added plan

--- src/services/plan/plan.controller.ts
+++ src/services/plan/plan.controller.ts
@@ -1,16 +1,25 @@
 import { Controller, Get, Put, Post, Delete, Param, Body } from '@nestjs/common';
+import { PlanService } from './plan.service';
 
 @Controller('plans')
 export class PlanController {
 
+    constructor(private readonly PlanService: PlanService){}
+
     @Get()
-    getAll(){}
+    getAll(){
+        return this.PlanService.getAll()
+    }
 
     @Get('id/:idSpot')
-    getById(@Param('idSpot') idSpot: string){}
+    getById(@Param('idSpot') idSpot: string){
+        return this.PlanService.getById(idSpot);
+    }
 
     @Get('name/:nameSpot')
-    getByName(@Param('nameSpot') nameSpot: string){}
+    getByName(@Param('nameSpot') nameSpot: string){
+        return this.PlanService.getByName(nameSpot)
+    }
 
     @Post()
     createPlan(){}
