7ee3e6e0801af3a4c979c3eefb0f67a5124422de
MoisoDavid <wollxi@gmail.com>
2023-03-28 09:30:06 -0500

    Service to query by name plans

--- src/services/plan/plan.service.ts
+++ src/services/plan/plan.service.ts
@@ -9,5 +9,48 @@ export class PlanService {
     constructor(
         @InjectModel('plan') private readonly planModel: Model<Plan>,
         @InjectModel('spot') private readonly spotModel: Model<Spot>
-        ){}
+    ){};
+
+    async getAll(){
+        try {
+            const plans = await this.planModel.find();
+
+            if(plans.length === 0){
+                return {message: `Plans not found`}
+            }
+
+            return plans  
+        } catch (error) {
+            return {message: 'An unexpected error ocurred on DB', error}
+        }
+    };
+
+    async getById(idPlan: string){
+        try {
+            const plan = await this.planModel.findById(idPlan)
+
+            if(!plan){
+                return {message: `Plan with id: ${idPlan} not found`}
+            }
+
+            return plan
+        } catch (error) {
+            return {message: 'An unexpected error ocurred on DB', error}
+        }
+    }
+
+    async getByName(namePlan: string){
+        try {
+            const plan = await this.planModel.findOne({name: namePlan});
+
+            if(!plan){
+                return {message: `Plan with name: ${namePlan} not found`}
+            }
+
+            return plan
+        } catch (error) {
+            return {message: 'An unexpected error ocurred on DB', error};
+        }
+    }
+    
 }
